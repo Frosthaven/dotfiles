1ab4664f5fc54a6310e3b1923f0a6a39c07b3aa6
Frosthaven <frosthaven@thedragon.dev>
2025-01-27 13:33:02 -0500

    added mini.files

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -164,6 +164,10 @@ require('lazy').setup({
     event = 'VimEnter',
     config = true,
   },
+  { -- Adds mini.files for editing the filesystem
+    'echasnovski/mini.files',
+    version = false,
+  },
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
@@ -926,6 +930,69 @@ require('lazy').setup({
 
 -- Custom Code
 
+-- [[
+-- enable mini.files
+-- ]]
+
+require('mini.files').setup {
+  -- Customization of shown content
+  content = {
+    -- Predicate for which file system entries to show
+    filter = nil,
+    -- What prefix to show to the left of file system entry
+    prefix = nil,
+    -- In which order to show file system entries
+    sort = nil,
+  },
+
+  -- Module mappings created only inside explorer.
+  -- Use `''` (empty string) to not create one.
+  mappings = {
+    close = 'q',
+    go_in = '<right>',
+    go_in_plus = 'L',
+    go_out = '<left>',
+    go_out_plus = 'H',
+    mark_goto = "'",
+    mark_set = 'm',
+    reset = '<BS>',
+    reveal_cwd = '@',
+    show_help = 'g?',
+    synchronize = '=',
+    trim_left = '<',
+    trim_right = '>',
+  },
+
+  -- General options
+  options = {
+    -- Whether to delete permanently or move into module-specific trash
+    permanent_delete = true,
+    -- Whether to use for editing directories
+    use_as_default_explorer = true,
+  },
+
+  -- Customization of explorer windows
+  windows = {
+    -- Maximum number of windows to show side by side
+    max_number = math.huge,
+    -- Whether to show preview of file/directory under cursor
+    preview = true,
+    -- Width of focused window
+    width_focus = 50,
+    -- Width of non-focused window
+    width_nofocus = 15,
+    -- Width of preview window
+    width_preview = 25,
+  },
+}
+
+vim.keymap.set('n', '-', function()
+  local MiniFiles = require 'mini.files'
+  local _ = MiniFiles.close() or MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+  vim.defer_fn(function()
+    MiniFiles.reveal_cwd()
+  end, 30)
+end, { desc = 'Open file browser' })
 --[[
     hijack netrw for local files
 --]]
