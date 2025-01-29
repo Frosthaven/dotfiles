889b8b2208a3cf3dcce6c7d759aa8548fa818d30
Frosthaven <frosthaven@thedragon.dev>
2025-01-29 12:42:41 -0500

    autocomplete: changed to blink engine

--- dot_config/nvim/lua/custom/plugins/autocomplete.lua
+++ dot_config/nvim/lua/custom/plugins/autocomplete.lua
@@ -1,4 +1,20 @@
 return {
+
+    { -- Autocompletion nvim-cmp alternative
+        'saghen/blink.cmp',
+        dependencies = 'rafamadriz/friendly-snippets',
+        version = 'v0.*',
+        opts = {
+            keymap = { preset = 'default' },
+            appearance = {
+                use_nvim_cmp_as_default = true,
+                nerd_font_variant = 'mono',
+            },
+            signature = { enabled = true },
+        },
+    },
+
+    --[[
     { -- Autocompletion
         'hrsh7th/nvim-cmp',
         event = 'InsertEnter',
@@ -114,4 +130,5 @@ return {
             }
         end,
     },
+    --]]
 }
