90c01ceccdc33b33b5f050c000a5f7e125be5c3a
Frosthaven <frosthaven@thedragon.dev>
2025-02-06 11:06:51 -0500

    added toggleterm

---
+++ dot_config/nvim/lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,25 @@
+return {
+    {
+        'akinsho/toggleterm.nvim',
+        version = '*',
+        config = function()
+            require('toggleterm').setup {
+                size = 10,
+                direction = 'float',
+                float_opts = {
+                    border = 'single',
+                    width = 75,
+                    height = 20,
+                    --highlights = {
+                    --     border = 'Normal',
+                    --  background = 'Normal',
+                    --},
+                },
+                border = 'curved',
+                open_mapping = [[<leader>tt]],
+                hide_numbers = true,
+                shade_terminals = true,
+            }
+        end,
+    },
+}
