08d61db062919a347e7fb7877200877eccc4c74f
Frosthaven <frosthaven@thedragon.dev>
2025-02-10 21:13:16 -0500

    initial komorebi+whkdrc config

---
+++ run_after__symlink-komorebi_windows.ps1
@@ -0,0 +1,23 @@
+# This links Window's komorebi config to the location Unix uses.
+#
+# This is necessary because Windows uses a different location for config than
+# Unix does, and we want to keep the config in the same location for all
+# platforms.
+
+If ($PSVersionTable.PSVersion.Major -Le 5 -Or $isWindows) {
+    # komorebi file links
+    If (-Not (Test-Path $env:USERPROFILE\komorebi.json)) {
+        New-Item -Path $env:USERPROFILE\komorebi.json -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.json
+    }
+    If (-Not (Test-Path $env:USERPROFILE\komorebi.ps1)) {
+        New-Item -Path $env:USERPROFILE\komorebi.ps1 -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.ps1
+    }
+    If (-Not (Test-Path $env:USERPROFILE\komorebi.generated.ps1)) {
+        New-Item -Path $env:USERPROFILE\komorebi.generated.ps1 -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.generated.ps1
+    }
+
+    # komorebi bar file links
+    If (-Not (Test-Path $env:USERPROFILE\komorebi.bar.json)) {
+        New-Item -Path $env:USERPROFILE\komorebi.bar.json -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.bar.json
+    }
+}
