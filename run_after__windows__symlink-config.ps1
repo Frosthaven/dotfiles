eba1ee59c926278d2318fbea274669a82be62c79
Frosthaven <frosthaven@thedragon.dev>
2025-02-13 04:38:35 -0500

    normalized macos name in package installer

--- run_after__windows__symlink-config.ps1
+++ run_after__windows__symlink-config.ps1
@@ -1,11 +1,22 @@
-# This links Window's komorebi config to the location Unix uses.
+# This links Window's configuration files back to the ~/.config directory.
 #
 # This is necessary because Windows uses a different location for config than
 # Unix does, and we want to keep the config in the same location for all
-# platforms.
+# platforms to keep them maintainable.
 
 If ($PSVersionTable.PSVersion.Major -Le 5 -Or $isWindows) {
-    # komorebi file links
+
+    # Alacritty
+    If (-Not (Test-Path $env:APPDATA\Alacritty)) {
+        New-Item -Path $env:APPDATA\Alacritty -ItemType Junction -Value $env:USERPROFILE\.config/alacritty
+    }
+
+    # Neovim
+    If (-Not (Test-Path $env:LOCALAPPDATA\nvim)) {
+        New-Item -Path $env:LOCALAPPDATA\nvim -ItemType Junction -Value $env:USERPROFILE\.config/nvim
+    }
+
+    # Komorebi
     If (-Not (Test-Path $env:USERPROFILE\komorebi.json)) {
         New-Item -Path $env:USERPROFILE\komorebi.json -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.json
     }
@@ -15,12 +26,8 @@ If ($PSVersionTable.PSVersion.Major -Le 5 -Or $isWindows) {
     If (-Not (Test-Path $env:USERPROFILE\komorebi.generated.ps1)) {
         New-Item -Path $env:USERPROFILE\komorebi.generated.ps1 -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.generated.ps1
     }
-    #If (-Not (Test-Path $env:USERPROFILE\komorebi.ahk)) {
-    #    New-Item -Path $env:USERPROFILE\komorebi.ahk -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.ahk
-    #}
-
-    # komorebi bar file links
     If (-Not (Test-Path $env:USERPROFILE\komorebi.bar.json)) {
         New-Item -Path $env:USERPROFILE\komorebi.bar.json -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.bar.json
     }
+
 }
