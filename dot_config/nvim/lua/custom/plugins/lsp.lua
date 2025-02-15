9da73508ad9341773f7db5b4c986569da771c633
Frosthaven <frosthaven@thedragon.dev>
2025-02-14 22:26:00 -0500

    initial nvm install changes

--- dot_config/nvim/lua/custom/plugins/lsp.lua
+++ dot_config/nvim/lua/custom/plugins/lsp.lua
@@ -174,7 +174,15 @@ return {
                 -- clangd = {},
                 gopls = {},
                 pyright = {},
-                twiggy_language_server = {},
+                twiggy_language_server = {
+                    settings = {
+                        twiggy = {
+                            phpExecutable = 'php',
+                            framework = 'symfony',
+                            symfonyConsolePath = 'bin/console',
+                        },
+                    },
+                },
                 -- rust_analyzer = {},
                 -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
                 --
