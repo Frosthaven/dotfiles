02e3f427e9f4367aae6d90efccc9a897cddc901e
Frosthaven <frosthaven@thedragon.dev>
2025-02-17 17:45:10 -0500

    nu in vim

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -29,16 +29,81 @@ M.setup = function()
         vim.opt.clipboard = 'unnamedplus'
     end)
 
-    -- If on windows, configure shell to use Powershell
-    if vim.fn.has 'win32' == 1 then
-        vim.opt.shell = vim.fn.executable 'pwsh' and 'pwsh' or 'powershell'
-        vim.opt.shellcmdflag =
-            '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
-        vim.opt.shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
-        vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-        vim.opt.shellquote = ''
-        vim.opt.shellxquote = ''
+    -- -- If on windows, configure shell to use Powershell
+    -- vim.opt.shell = vim.fn.executable 'nu' and 'nu' or 'zsh'
+    -- if vim.fn.has 'win32' == 1 then
+    --     vim.opt.shell = vim.fn.executable 'nu' and 'nu' or 'pwsh'
+    --     if vim.opt.shell == 'pwsh' then
+    --         vim.opt.shellcmdflag =
+    --             '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+    --         vim.opt.shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
+    --         vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    --         vim.opt.shellquote = ''
+    --         vim.opt.shellxquote = ''
+    --     end
+    --     if vim.opt.shell == 'nu' then
+    --         vim.opt.shellcmdflag = '-c'
+    --         vim.opt.shellQuote = ''
+    --         vim.opt.shellxquote = ''
+    --     end
+    -- end
+
+    -- better nu support in nvim
+    -- https://www.kiils.dk/en/blog/2024-06-22-using-nushell-in-neovim/
+    local posix_shell_options = {
+        shellcmdflag = '-c',
+        shellpipe = '2>&1 | tee',
+        shellquote = '',
+        shellredir = '>%s 2>&1',
+        shelltemp = true,
+        shellxescape = '',
+        shellxquote = '',
+    }
+
+    local nu_shell_options = {
+        shellcmdflag = '--login --stdin --no-newline -c',
+        shellpipe = '| complete | update stderr { ansi strip } | tee { get stderr | save --force --raw %s } | into record',
+        shellquote = '',
+        shellredir = 'out+err> %s',
+        shelltemp = false,
+        shellxescape = '',
+        shellxquote = '',
+    }
+
+    local function set_options(options)
+        for k, v in pairs(options) do
+            vim.opt[k] = v
+        end
     end
+
+    local function apply_shell_options()
+        -- check if the shell ends with "nu"
+        if vim.opt.shell:get():match 'nu$' ~= nil then
+            set_options(nu_shell_options)
+        else
+            set_options(posix_shell_options)
+        end
+    end
+
+    if vim.fn.executable 'nu' then
+        vim.opt.shell = 'nu'
+    elseif vim.fn.executable 'pwsh' then
+        vim.opt.shell = 'pwsh'
+    elseif vim.fn.executable 'powershell' then
+        vim.opt.shell = 'powershell'
+    else
+        vim.opt.shell = 'zsh'
+    end
+
+    apply_shell_options()
+
+    -- listen for changes to the shell option
+    vim.api.nvim_create_autocmd('OptionSet', {
+        pattern = 'shell',
+        callback = function()
+            apply_shell_options()
+        end,
+    })
 
     -- Enable break indent
     vim.opt.breakindent = true
