542095650fb850602373292d204ced7e49061db7
Frosthaven <frosthaven@thedragon.dev>
2025-02-17 21:42:47 -0500

    package maintenance

--- dot_config/shell/zsh/chezmoi-zsh.sh
+++ dot_config/shell/zsh/chezmoi-zsh.sh
@@ -23,8 +23,3 @@ alias vi='nvim'
 eval "$(zoxide init zsh)"
 alias cd=z # replace cd with zoxide
 
-# nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm
-
