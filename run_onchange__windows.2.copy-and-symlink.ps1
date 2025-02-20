95d0c0d8393b7ce995fef59e9bccd3322a77ca99
Frosthaven <frosthaven@thedragon.dev>
2025-02-19 21:55:22 -0500

    package fixes

--- run_onchange__windows.2.copy-and-symlink.ps1
+++ run_onchange__windows.2.copy-and-symlink.ps1
@@ -3,63 +3,63 @@
 
 If ($PSVersionTable.PSVersion.Major -Le 5 -Or $isWindows) {
     Write-Host ""
-    Write-Host "- Checking for Junctions and Symbolic Links..."
+    Write-Host "Checking for symbolic links and integrations..."
 
     # Alacritty
     If ((Get-Item $env:APPDATA\Alacritty).Attributes -ne "ReparsePoint") {
-        Remove-Item $env:APPDATA\Alacritty -Recurse -Force -Confirm:$false
-        New-Item -Path $env:APPDATA\Alacritty -ItemType Junction -Value $env:USERPROFILE\.config/alacritty
+        Remove-Item $env:APPDATA\Alacritty -Recurse -Force -Confirm:$false | Out-Null
+        New-Item -Path $env:APPDATA\Alacritty -ItemType Junction -Value $env:USERPROFILE\.config/alacritty | Out-Null
     }
 
     # Neovim
     If ((Get-Item $env:LOCALAPPDATA\nvim).Attributes -ne "ReparsePoint") {
-        Remove-Item $env:LOCALAPPDATA\nvim -Recurse -Force -Confirm:$false
-        New-Item -Path $env:LOCALAPPDATA\nvim -ItemType Junction -Value $env:USERPROFILE\.config/nvim
+        Remove-Item $env:LOCALAPPDATA\nvim -Recurse -Force -Confirm:$false | Out-Null
+        New-Item -Path $env:LOCALAPPDATA\nvim -ItemType Junction -Value $env:USERPROFILE\.config/nvim | Out-Null
     }
 
     # Komorebi
     If ((Get-Item $env:USERPROFILE\komorebi.json).Attributes -ne "ReparsePoint") {
-        Remove-Item $env:USERPROFILE\komorebi.json -Recurse -Force -Confirm:$false
-        New-Item -Path $env:USERPROFILE\komorebi.json -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.json
+        Remove-Item $env:USERPROFILE\komorebi.json -Recurse -Force -Confirm:$false | Out-Null
+        New-Item -Path $env:USERPROFILE\komorebi.json -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.json | Out-Null
     }
     If ((Get-Item $env:USERPROFILE\komorebi.ps1).Attributes -ne "ReparsePoint") {
-        Remove-Item $env:USERPROFILE\komorebi.ps1 -Recurse -Force -Confirm:$false
-        New-Item -Path $env:USERPROFILE\komorebi.ps1 -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.ps1
+        Remove-Item $env:USERPROFILE\komorebi.ps1 -Recurse -Force -Confirm:$false | Out-Null
+        New-Item -Path $env:USERPROFILE\komorebi.ps1 -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.ps1 | Out-Null
     }
     If ((Get-Item $env:USERPROFILE\komorebi.generated.ps1).Attributes -ne "ReparsePoint") {
-        Remove-Item $env:USERPROFILE\komorebi.generated.ps1 -Recurse -Force -Confirm:$false
-        New-Item -Path $env:USERPROFILE\komorebi.generated.ps1 -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.generated.ps1
+        Remove-Item $env:USERPROFILE\komorebi.generated.ps1 -Recurse -Force -Confirm:$false | Out-Null
+        New-Item -Path $env:USERPROFILE\komorebi.generated.ps1 -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.generated.ps1 | Out-Null
     }
     If ((Get-Item $env:USERPROFILE\komorebi.bar.json).Attributes -ne "ReparsePoint") {
-        Remove-Item $env:USERPROFILE\komorebi.bar.json -Recurse -Force -Confirm:$false
-        New-Item -Path $env:USERPROFILE\komorebi.bar.json -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.bar.json
+        Remove-Item $env:USERPROFILE\komorebi.bar.json -Recurse -Force -Confirm:$false | Out-Null
+        New-Item -Path $env:USERPROFILE\komorebi.bar.json -ItemType SymbolicLink -Value $env:USERPROFILE\.config\komorebi\komorebi.bar.json | Out-Null
     }
 
     # Clink Scripts
     If (-Not (Test-Path $env:LOCALAPPDATA\clink)) {
-        New-Item -Path $env:LOCALAPPDATA\clink -ItemType Directory
+        New-Item -Path $env:LOCALAPPDATA\clink -ItemType Directory | Out-Null
     }
-    Copy-Item -Path $env:USERPROFILE\.config\clink\* -Destination $env:LOCALAPPDATA\clink -Recurse -Force
+    Copy-Item -Path $env:USERPROFILE\.config\clink\* -Destination $env:LOCALAPPDATA\clink -Recurse -Force | Out-Null
 
     # Automatic Shell Integration
     If (-Not (Test-Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1)) {
-        New-Item -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -ItemType File
+        New-Item -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -ItemType File | Out-Null
     }
     If (-Not (Select-String -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Pattern "chezmoi-powershell.ps1")) {
-        Add-Content -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Value "# Load Chezmoi PowerShell profile"
-        Add-Content -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Value ". '$env:USERPROFILE\.config\shell\powershell\chezmoi-powershell.ps1'"
+        Add-Content -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Value "# Load Chezmoi PowerShell profile" | Out-Null
+        Add-Content -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Value ". '$env:USERPROFILE\.config\shell\powershell\chezmoi-powershell.ps1'" | Out-Null
     }
 
     # Automatic Zoxide Integration
     If (-Not (Select-String -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Pattern "chezmoi-powershell-zoxide.ps1")) {
-        Add-Content -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Value "# Load Zoxide PowerShell profile"
-        Add-Content -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Value ". '$env:USERPROFILE\.config\shell\powershell\chezmoi-powershell-zoxide.ps1'"
+        Add-Content -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Value "# Load Zoxide PowerShell profile" | Out-Null
+        Add-Content -Path $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Value ". '$env:USERPROFILE\.config\shell\powershell\chezmoi-powershell-zoxide.ps1'" | Out-Null
     }
 
     # Nushell
     If ((Get-Item $env:APPDATA\nushell).Attributes -ne "ReparsePoint") {
-        Remove-Item $env:APPDATA\nushell -Recurse -Force -Confirm:$false
-        New-Item -Path $env:APPDATA\nushell -ItemType Junction -Value $env:USERPROFILE\.config\shell\nushell
+        Remove-Item $env:APPDATA\nushell -Recurse -Force -Confirm:$false | Out-Null
+        New-Item -Path $env:APPDATA\nushell -ItemType Junction -Value $env:USERPROFILE\.config\shell\nushell | Out-Null
     }
 }
 
