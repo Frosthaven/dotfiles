91b11c2a3b27085be7df6eb16ddc7136a2686541
Frosthaven <frosthaven@thedragon.dev>
2025-02-21 08:00:46 -0500

    powershell: added sf command

--- dot_config/shell/powershell/chezmoi-powershell.ps1
+++ dot_config/shell/powershell/chezmoi-powershell.ps1
@@ -47,6 +47,18 @@ if (-not $env:FNM_DIR) {
 # UV binaries
 $env:Path += ";$env:USERPROFILE\.local\bin"
 
+# fzf - [s]earch [f]iles command (sf)
+# this will use the output of the fzf command to open the file in nvim. You can
+# change the editor launch command to whatever you want.
+# see: https://github.com/junegunn/fzf
+# see: https://github.com/sharkdp/bat
+function sf {
+    $file = fzf --preview "bat --color=always {}" --preview-window=right:50%:wrap --height 50% --border --prompt="Search Files: " --query="$args"
+    if ($file) {
+        nvim $file # or with your editor of choice
+    }
+}
+
 # ALIASES *********************************************************************
 # *****************************************************************************
 function docmd { cmd /c $args }
