71c2daaa72167f7fbf3f6e978d27e6e1059edef7
Frosthaven <frosthaven@thedragon.dev>
2025-02-25 22:40:57 -0500

    nvim: shell config optimization

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,4 +1,5 @@
 require('config.keymap').setup()
 require('config.options').setup()
+require('config.shell').setup()
 require('config.autocommands').setup()
 require('config.plugin-loader').setup() -- using lazy
