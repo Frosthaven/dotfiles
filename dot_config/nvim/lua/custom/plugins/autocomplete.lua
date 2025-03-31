831e6d7fcde4dfc657df05ca6c75aa7f8acfa4d3
Frosthaven <frosthaven@thedragon.dev>
2025-03-30 23:31:15 -0400

    feat(blink-cmp v1.0): cleaned up autocomplete code

--- dot_config/nvim/lua/custom/plugins/autocomplete.lua
+++ dot_config/nvim/lua/custom/plugins/autocomplete.lua
@@ -3,15 +3,20 @@ return {
     { -- Autocompletion nvim-cmp alternative
         'saghen/blink.cmp',
         dependencies = 'rafamadriz/friendly-snippets',
-        version = '*',
+        version = '1.*',
         opts = {
             keymap = { preset = 'default' },
             appearance = {
-                use_nvim_cmp_as_default = true,
                 nerd_font_variant = 'mono',
             },
+            completion = { documentation = { auto_show = true } },
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
             signature = { enabled = true },
+            fuzzy = { implementation = 'prefer_rust_with_warning' },
         },
+        opts_extend = { 'sources.default' },
     },
 
     -- { -- Autocompletion
