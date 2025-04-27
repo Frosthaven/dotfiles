b0ebea5ea747231ca362c715e252c816f0516801
Frosthaven <frosthaven@thedragon.dev>
2025-04-27 15:30:20 -0400

    feat(plugin: autotag): added html autotagging

---
+++ dot_config/nvim/lua/custom/plugins/autotag.lua
@@ -0,0 +1,11 @@
+return {
+    'windwp/nvim-ts-autotag',
+    ft = { -- filetypes enabled for
+        'javascript',
+        'javascriptreact',
+        'typescript',
+        'typescriptreact',
+        'html',
+        'twig',
+    },
+}
