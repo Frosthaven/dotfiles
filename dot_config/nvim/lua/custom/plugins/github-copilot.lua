c32a44252f1f2be06ee8fc7ff439c9b7527ff4cf
Frosthaven <frosthaven@thedragon.dev>
2025-08-10 05:18:04 -0400

    feat(copilot): remove version lock on copilot (issue fixed)

--- dot_config/nvim/lua/custom/plugins/github-copilot.lua
+++ dot_config/nvim/lua/custom/plugins/github-copilot.lua
@@ -2,6 +2,6 @@ return {
     { -- github copilot
         'github/copilot.vim',
         -- version lock reason: bug in version 1.42.0
-        version = '1.34.0',
+        -- version = '1.34.0',
     },
 }
