99ca540888c1534727d33d44328ca19152d70f57
Frosthaven <frosthaven@thedragon.dev>
2025-08-10 20:49:30 -0400

    misc fixes

--- dot_config/nvim/lua/custom/plugins/lsp.lua
+++ dot_config/nvim/lua/custom/plugins/lsp.lua
@@ -177,6 +177,15 @@ return {
                 lua_ls = { -- lua
                     settings = {
                         Lua = {
+                            runtime = {
+                                version = 'LuaJIT',
+                            },
+                            diagnostics = {
+                                globals = { 'vim' },
+                            },
+                            workspace = {
+                                library = vim.api.nvim_get_runtime_file('', true),
+                            },
                             completion = {
                                 callSnippet = 'Replace',
                             },
