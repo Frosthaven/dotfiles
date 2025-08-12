97264050113e12cca4c58708c6d0e9dac5910863
Frosthaven <frosthaven@thedragon.dev>
2025-08-12 13:52:26 -0400

    chore: cleanup dated comments

--- dot_config/nvim/lua/custom/plugins/github-copilot.lua
+++ dot_config/nvim/lua/custom/plugins/github-copilot.lua
@@ -1,8 +1,6 @@
 return {
     { -- github copilot
         'github/copilot.vim',
-        -- version lock reason: bug in version 1.42.0
-        -- version = '1.34.0',
         config = function()
             -- remap completion to ctrl+shift+y
             vim.g.copilot_no_tab_map = true
