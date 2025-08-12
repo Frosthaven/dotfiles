302bf5159f769bc0d7f7139b05920ee9846ffcb4
Frosthaven <frosthaven@thedragon.dev>
2025-08-12 13:51:17 -0400

    feat(copilot:keymap): changed from tab completion to ctrl+shift+y

--- dot_config/nvim/lua/custom/plugins/github-copilot.lua
+++ dot_config/nvim/lua/custom/plugins/github-copilot.lua
@@ -3,5 +3,10 @@ return {
         'github/copilot.vim',
         -- version lock reason: bug in version 1.42.0
         -- version = '1.34.0',
+        config = function()
+            -- remap completion to ctrl+shift+y
+            vim.g.copilot_no_tab_map = true
+            vim.api.nvim_set_keymap('i', '<C-S-y>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+        end,
     },
 }
