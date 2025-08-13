4ec5f57d998eea7f7d8f352a3f046c6fbb684f67
Frosthaven <frosthaven@thedragon.dev>
2025-08-13 11:24:24 -0400

    fix(nvim:copilot): remove right arrow autocomplete

--- dot_config/nvim/lua/custom/plugins/github-copilot.lua
+++ dot_config/nvim/lua/custom/plugins/github-copilot.lua
@@ -2,10 +2,9 @@ return {
     { -- github copilot
         'github/copilot.vim',
         config = function()
-            -- remap completion to shift+tab or right arrow
+            -- remap completion to shift+tab
             vim.g.copilot_no_tab_map = true
             vim.api.nvim_set_keymap('i', '<S-Tab>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
-            vim.api.nvim_set_keymap('i', '<Right>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
         end,
     },
 }
