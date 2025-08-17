b116fa9f9a92406a889bbd1afd027cb0b767158f
Frosthaven <shane@thedragon.dev>
2025-08-17 03:34:23 -0400

    neovim/wezterm chroming update

--- dot_config/nvim/lua/custom/plugins/appearance.lua
+++ dot_config/nvim/lua/custom/plugins/appearance.lua
@@ -7,8 +7,15 @@ return {
             require('virt-column').setup {
                 char = '│',
                 virtcolumn = '80,120',
-                highlight = 'LineNr',
+                highlight = 'VirtColumn',
             }
+
+            -- make line number colors #11111D
+            -- and #11111D for the virt column
+            vim.api.nvim_set_hl(0, 'LineNr', { fg = '#656588', bg = '#11111D' })
+            vim.api.nvim_set_hl(0, 'VirtColumn', { fg = '#252548' })
+            vim.api.nvim_set_hl(0, 'SignColumn', { bg = '#11111D' })
+            vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#202037' })
         end,
     },
     {
