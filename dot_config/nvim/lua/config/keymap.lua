bbad0ce06db1b91ca53a1816f5ff1c0e0c392899
Frosthaven <shane@thedragon.dev>
2025-08-17 04:04:08 -0400

    feat(neovim): keymap / diagnostic updates

--- dot_config/nvim/lua/config/keymap.lua
+++ dot_config/nvim/lua/config/keymap.lua
@@ -25,15 +25,15 @@ M.setup = function()
     end
 
     vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-    -- vim.keymap.set('n', '<leader>dt', function()
-    --     if vim.diagnostic.config().virtual_lines == true then
-    --         vim.diagnostic.config { virtual_lines = { current_line = true } }
-    --         vim.notify 'Global diagnostic virtual lines disabled'
-    --     else
-    --         vim.diagnostic.config { virtual_lines = true }
-    --         vim.notify 'Global diagnostic virtual lines enabled'
-    --     end
-    -- end, { desc = 'Toggle [D]iagnostic [T]oggle' })
+    vim.keymap.set('n', '<leader>dt', function()
+        if vim.diagnostic.config().virtual_text == true then
+            vim.diagnostic.config { virtual_text = false, virtual_lines = false }
+            vim.notify 'Global diagnostic virtual lines disabled'
+        else
+            vim.diagnostic.config { virtual_text = true, virtual_lines = false }
+            vim.notify 'Global diagnostic virtual lines enabled'
+        end
+    end, { desc = 'Toggle [D]iagnostic [T]oggle' })
     -- create a diagnostic popup if you press leader d k
     vim.keymap.set('n', '<leader>dd', function()
         local opts = {
@@ -82,21 +82,11 @@ M.setup = function()
     --  Use CTRL+<hjkl> to switch between windows
     --
 
-    -- Check if the quickfix list is open and has entries
-    local function is_quickfix_open()
-        local qflist = vim.fn.getqflist { id = 0 }
-        return #qflist > 0
-    end
-
     -- VIM Style Pane Management
     vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
     vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
     vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
     vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
-    -- Conditional quickfix or diagnostic navigation (M-j, M-k)
-    vim.keymap.set('n', '<M-j>', '<cmd>cnext<CR>', { desc = 'Move to the next quickfix or diagnostic item' })
-    vim.keymap.set('n', '<M-k>', '<cmd>cprev<CR>', { desc = 'Move to the previous quickfix or diagnostic item' })
 
     -- Arrow Style Pane Management
     vim.keymap.set('n', '<C-Up>', '<C-w><Up>', { desc = 'Move focus to the upper window' })
@@ -104,6 +94,9 @@ M.setup = function()
     vim.keymap.set('n', '<C-Left>', '<C-w><Left>', { desc = 'Move focus to the left window' })
     vim.keymap.set('n', '<C-Right>', '<C-w><Right>', { desc = 'Move focus to the right window' })
 
+    -- quickfix navigation (M-j, M-k)
+    vim.keymap.set('n', '<M-j>', '<cmd>cnext<CR>', { desc = 'Move to the next quickfix or diagnostic item' })
+    vim.keymap.set('n', '<M-k>', '<cmd>cprev<CR>', { desc = 'Move to the previous quickfix or diagnostic item' })
     -- quickfix navigation (M-Down, M-Up)
     vim.keymap.set('n', '<M-Down>', '<cmd>cnext<CR>', { desc = 'Move to the next quickfix item' })
     vim.keymap.set('n', '<M-Up>', '<cmd>cprev<CR>', { desc = 'Move to the previous quickfix item' })
