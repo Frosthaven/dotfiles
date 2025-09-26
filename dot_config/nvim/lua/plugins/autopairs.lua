05c28b31893f82e62c1a701913681093413f4366
Frosthaven <shane@thedragon.dev>
2025-09-26 17:01:00 -0400

    feat(nvim:autopairs): some twig support

--- dot_config/nvim/lua/plugins/autopairs.lua
+++ dot_config/nvim/lua/plugins/autopairs.lua
@@ -1,7 +1,36 @@
 return {
     'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
+    event = 'InsertEnter',
+    config = function()
+        local npairs = require 'nvim-autopairs'
+        local Rule = require 'nvim-autopairs.rule'
+
+        npairs.setup {
+            check_ts = true,
+        }
+
+        -- Twig {%  %}
+        npairs.add_rule(Rule('{%', '%}')
+            :with_pair(function()
+                return true
+            end)
+            :replace_endpair(function()
+                -- existing pairing rules already created } by this point
+                -- so we just need to add the ' %' before the generated }
+                return ' %'
+            end)
+            :use_key '%')
+
+        -- Twig {#  #}
+        npairs.add_rule(Rule('{#', '#}')
+            :with_pair(function()
+                return true
+            end)
+            :replace_endpair(function()
+                -- existing pairing rules already created } by this point
+                -- so we just need to add the ' #' before the generated }
+                return ' #'
+            end)
+            :use_key '#')
+    end,
 }
