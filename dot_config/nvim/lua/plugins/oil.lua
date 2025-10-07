b723f932093fb97b127a437b70dbf3f5f0bdd66b
Frosthaven <frosthaven@thedragon.dev>
2025-10-07 11:52:12 -0400

    feat(nvim:oil): setup configs

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,5 +1,10 @@
 return {
     "stevearc/oil.nvim",
     enabled = true,
-    opts = {},
+    opts = {
+        default_file_explorer = false,
+        view_options = {
+            show_hidden = true,
+        }
+    },
 }
