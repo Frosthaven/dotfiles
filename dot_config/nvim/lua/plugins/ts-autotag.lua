335c5dc41e297f9b5b7e86242bff18f61823dfc2
Frosthaven <frosthaven@thedragon.dev>
2025-12-29 11:49:57 -0500

    fix(nvim): use new nvim-ts-autotag setup format

--- dot_config/nvim/lua/plugins/ts-autotag.lua
+++ dot_config/nvim/lua/plugins/ts-autotag.lua
@@ -2,9 +2,11 @@ return {
     'windwp/nvim-ts-autotag',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     opts = {
-        enable_close = true,
-        enable_rename = true,
-        enable_close_on_slash = true,
+        opts = {
+            enable_close = true,
+            enable_rename = true,
+            enable_close_on_slash = false,
+        },
         per_filetype = {
             -- ['html'] = {
             --     enable_close = false,
