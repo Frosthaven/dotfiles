9fe54832636e250e82afba3acae75d732443140a
Frosthaven <shane@thedragon.dev>
2026-01-17 21:05:57 -0500

    fix: use local node_modules biome for LSP

--- dot_config/nvim/lsp/biome.lua
+++ dot_config/nvim/lsp/biome.lua
@@ -1,5 +1,19 @@
 return {
-    cmd = { "biome", "lsp-proxy" },
+    cmd = function()
+        -- Try to find project-local biome first
+        local root = vim.fs.find({ 'biome.json', 'biome.jsonc' }, {
+            upward = true,
+            path = vim.fn.getcwd(),
+        })[1]
+        if root then
+            local project_dir = vim.fn.fnamemodify(root, ':h')
+            local local_biome = project_dir .. '/node_modules/.bin/biome'
+            if vim.fn.executable(local_biome) == 1 then
+                return { local_biome, 'lsp-proxy' }
+            end
+        end
+        return { 'biome', 'lsp-proxy' }
+    end,
     filetypes = {
         "javascript",
         "javascriptreact",
