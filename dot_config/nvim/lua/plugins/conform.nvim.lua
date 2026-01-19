de84435e6cfa78bf73a1b1b33892242adb934cd4
Frosthaven <shane@thedragon.dev>
2026-01-19 09:28:08 -0500

    feat(nvim:conform): better biom checks

--- dot_config/nvim/lua/plugins/conform.nvim.lua
+++ dot_config/nvim/lua/plugins/conform.nvim.lua
@@ -34,16 +34,39 @@ return {
             end,
             formatters_by_ft = {
                 lua = { 'stylua' },
-                javascript = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-                javascriptreact = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-                typescript = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-                typescriptreact = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-                json = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-                jsonc = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-                css = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
+                javascript = { 'biome_check', 'prettierd', 'prettier', stop_after_first = true },
+                javascriptreact = { 'biome_check', 'prettierd', 'prettier', stop_after_first = true },
+                typescript = { 'biome_check', 'prettierd', 'prettier', stop_after_first = true },
+                typescriptreact = { 'biome_check', 'prettierd', 'prettier', stop_after_first = true },
+                json = { 'biome_check', 'prettierd', 'prettier', stop_after_first = true },
+                jsonc = { 'biome_check', 'prettierd', 'prettier', stop_after_first = true },
+                css = { 'biome_check', 'prettierd', 'prettier', stop_after_first = true },
             },
             formatters = {
                 biome = {
+                    command = function(self, ctx)
+                        local root = vim.fs.find({ 'biome.json', 'biome.jsonc' }, {
+                            upward = true,
+                            path = ctx and ctx.dirname or vim.fn.getcwd(),
+                        })[1]
+                        if root then
+                            local project_dir = vim.fn.fnamemodify(root, ':h')
+                            local local_biome = project_dir .. '/node_modules/.bin/biome'
+                            if vim.fn.executable(local_biome) == 1 then
+                                return local_biome
+                            end
+                        end
+                        return 'biome'
+                    end,
+                    condition = function(self, ctx)
+                        return vim.fs.find({ 'biome.json', 'biome.jsonc' }, {
+                            upward = true,
+                            path = ctx.dirname,
+                        })[1] ~= nil
+                    end,
+                },
+                biome_check = {
+                    args = { 'check', '--write', '--stdin-file-path', '$FILENAME' },
                     command = function(self, ctx)
                         local root = vim.fs.find({ 'biome.json', 'biome.jsonc' }, {
                             upward = true,
